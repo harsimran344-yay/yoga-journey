43ec5eb209d44d46f266ee708c01549179880a65
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-06-02 00:04:59 -0400

    add navigation with nuxt-link

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,5 +1,17 @@
+<script>
+import NavBar from '~/components/NavBar.vue'
+
+export default {
+    components: {
+        NavBar
+    }
+}
+</script>>
+
+
 <template>
     <div>
         This is Header
+        <NavBar />
     </div>
 </template>
--- client/components/NavBar.vue
+++ client/components/NavBar.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        This is Navigation Bar
+        <ul>
+            <li>
+                <nuxt-link to="/">HOME</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/about">ABOUT</nuxt-link>
+            </li>
+        </ul>
     </div>
 </template>
