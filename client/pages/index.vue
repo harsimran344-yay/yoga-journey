eb5165966bc33c83ad809e55c9b1b426c7340127
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-06-01 23:55:12 -0400

    setup content json files for home and about page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -6,12 +6,18 @@ export default {
   components: {
     YogaImage,
     YogaText
+  },
+
+  async asyncData ({ $content }) {
+    const page = await $content('index').fetch()
+    return { page }
   }
 }
 </script>
 
 <template>
   <div>
+    <h1>{{ page.description }}</h1>
     <YogaImage />
     <YogaText />
   </div>
