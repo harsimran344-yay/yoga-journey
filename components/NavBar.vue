49018d2f8fb3ed5d6b6b695554acf6a67094f698
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-06-06 17:11:28 -0400

    add nav bar content and links to home, about and contact us page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,15 +1,11 @@
 <template>
-    <div>
-        <ul>
-            <li>
-                <nuxt-link to="/">HOME</nuxt-link>
-            </li>
-            <li>
-                <nuxt-link to="/about">ABOUT</nuxt-link>
-            </li>
-            <li>
-                <nuxt-link to="/contact">CONTACT</nuxt-link>
-            </li>
-        </ul>
+    <div class="flex items-end">
+        <nav>
+            <!-- TODO: create common content for nav lists - home, about and contant. 
+            plane text will be deleted -->
+            <nuxt-link to="/">HOME</nuxt-link>
+            <nuxt-link to="/about">ABOUT</nuxt-link>
+            <nuxt-link to="/contact">CONTACT</nuxt-link>
+        </nav>
     </div>
 </template>
