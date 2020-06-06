965b6832c8027e51c61bc2325d5705eb64338825
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-06-06 17:10:40 -0400

    import navbar and logo img to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,17 @@
 <script>
-import NavBar from '~/components/NavBar.vue'
+    import NavBar from '~/components/NavBar.vue'
 
-export default {
-    components: {
-        NavBar
+    export default {
+        components: {
+            NavBar
+        }
     }
-}
 </script>>
 
-
 <template>
-    <div>
-        This is Header
+    <!-- TODO: setup a common content for header as well -->
+    <div class="px-10 pb-2 border-b-2 border-red-dark flex justify-between">
+        <img class="w-40 h-40" src="~/assets/images/yoga-logo.png" />
         <NavBar />
     </div>
 </template>
