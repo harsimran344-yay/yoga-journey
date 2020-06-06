18b53d53e7ab17e80935db927a7ea9eda51c5dbd
Harsimran <simran@Harsimrans-MacBook-Air.local>
2020-06-06 17:09:44 -0400

    add tailwind styles to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        This is Footer
+    <div class="px-10 border-t-2 border-red-dark flex justify-center">
+        <!-- TODO: setup a common content for footer -->
+        @copyright Yoga journey
     </div>
 </template>
-
